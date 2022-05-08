2f1fbb93c079b535dd8d36df459a0ec25768d41a
gsuaki <gabriel.suaki@mercadolivre.com>
2022-05-08 18:42:01 -0300

    3.02-adicionando-o-springboot-na-camada-de-infrastructure

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,14 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalogo")
+public class WebServerConfig {
+}
