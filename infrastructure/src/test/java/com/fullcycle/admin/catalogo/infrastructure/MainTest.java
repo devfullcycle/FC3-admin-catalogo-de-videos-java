c811fbdf4362cbe48dfb512c280a8ca1129bc3a8
gsuaki <gabriel.suaki@mercadolivre.com>
2022-05-08 19:44:35 -0300

    3.06-configurando-o-datasource-de-testes-da-aplicacao

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
@@ -2,11 +2,13 @@ package com.fullcycle.admin.catalogo.infrastructure;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
