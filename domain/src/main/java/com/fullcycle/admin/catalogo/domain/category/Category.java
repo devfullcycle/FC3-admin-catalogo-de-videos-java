40ff331f6f29bc31f62f8a41df77da305f738eaf
gsuaki <gabriel.suaki@mercadolivre.com>
2022-05-09 08:37:47 -0300

    4.08-testes-integrados-do-category-repository-para-cenarios-not-nullable

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -4,6 +4,7 @@ import com.fullcycle.admin.catalogo.domain.AggregateRoot;
 import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
 
 import java.time.Instant;
+import java.util.Objects;
 
 public class Category extends AggregateRoot<CategoryID> implements Cloneable {
     private String name;
@@ -26,8 +27,8 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         this.name = aName;
         this.description = aDescription;
         this.active = isActive;
-        this.createdAt = aCreationDate;
-        this.updatedAt = aUpdateDate;
+        this.createdAt = Objects.requireNonNull(aCreationDate, "'createdAt' should not be null");
+        this.updatedAt = Objects.requireNonNull(aUpdateDate, "'updatedAt' should not be null");
         this.deletedAt = aDeleteDate;
     }
 
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.fullcycle.admin.catalogo.infrastructure.category.persistence;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.MySQLGatewayTest;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAnInvalidNullName_whenCallsSave_shouldReturnError() {
+        final var expectedPropertyName = "name";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.name";
+
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+
+        final var anEntity = CategoryJpaEntity.from(aCategory);
+        anEntity.setName(null);
+
+        final var actualException =
+                Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+
+        final var actualCause =
+                Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+
+    @Test
+    public void givenAnInvalidNullCreatedAt_whenCallsSave_shouldReturnError() {
+        final var expectedPropertyName = "createdAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.createdAt";
+
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+
+        final var anEntity = CategoryJpaEntity.from(aCategory);
+        anEntity.setCreatedAt(null);
+
+        final var actualException =
+                Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+
+        final var actualCause =
+                Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+
+    @Test
+    public void givenAnInvalidNullUpdatedAt_whenCallsSave_shouldReturnError() {
+        final var expectedPropertyName = "updatedAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.updatedAt";
+
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
+
+        final var anEntity = CategoryJpaEntity.from(aCategory);
+        anEntity.setUpdatedAt(null);
+
+        final var actualException =
+                Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+
+        final var actualCause =
+                Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+}
