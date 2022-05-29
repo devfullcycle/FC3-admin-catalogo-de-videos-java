6578e99956abec7f5ccdb81c99bb440628782f93
gsuaki <gabriel.suaki@mercadolivre.com>
2022-05-29 17:42:20 -0300

    7.08-testes-de-regressao-manual-com-postman

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -9,7 +9,6 @@ import org.springframework.core.env.AbstractEnvironment;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPI.java
@@ -47,7 +47,6 @@ public interface CategoryAPI {
 
     @GetMapping(
             value = "{id}",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @Operation(summary = "Get a category by it's identifier")
@@ -73,7 +72,6 @@ public interface CategoryAPI {
 
     @DeleteMapping(
             value = "{id}",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseStatus(HttpStatus.NO_CONTENT)
