f251632d79e4a9f67a8149cf9e3d92e8ab5622ac
gsuaki <gabriel.suaki@mercadolivre.com>
2022-05-31 21:29:28 -0300

    8.02-testes-unitarios-da-nova-entidade-de-genre

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/genre/GenreTest.java
@@ -0,0 +1,83 @@
+package com.fullcycle.admin.catalogo.domain.genre;
+
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenreTest {
+
+    @Test
+    public void givenValidParams_whenCallNewGenre_shouldInstantiateAGenre() {
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        Assertions.assertNotNull(actualGenre);
+        Assertions.assertNotNull(actualGenre.getId());
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size());
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNullName_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidEmptyName_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        final var expectedName = """
+                Gostaria de enfatizar que o consenso sobre a necessidade de qualificação auxilia a preparação e a
+                composição das posturas dos órgãos dirigentes com relação às suas atribuições.
+                Do mesmo modo, a estrutura atual da organização apresenta tendências no sentido de aprovar a
+                manutenção das novas proposições.
+                """;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+}
