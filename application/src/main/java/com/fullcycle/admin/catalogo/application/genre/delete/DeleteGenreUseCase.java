17246d17a7622fd8b56749aa99a7f63061d6e617
gsuaki <gabriel.suaki@mercadolivre.com>
2022-06-08 18:50:58 -0300

    9.10-implementando-o-caso-de-uso-de-delete-de-categoria

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/delete/DeleteGenreUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalogo.application.genre.delete;
+
+import com.fullcycle.admin.catalogo.application.UnitUseCase;
+
+public abstract class DeleteGenreUseCase extends UnitUseCase<String> {
+}
