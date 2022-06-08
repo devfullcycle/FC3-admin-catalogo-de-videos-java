3f039d30dfa6ce9668e9698ee19c5bbf53dbb05e
gsuaki <gabriel.suaki@mercadolivre.com>
2022-06-08 18:02:57 -0300

    9.05-testes-unitarios-do-caso-de-uso-de-atualizacao-de-categoria

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/genre/update/UpdateGenreUseCase.java
@@ -0,0 +1,77 @@
+package com.fullcycle.admin.catalogo.application.genre.update;
+
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.genre.Genre;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateGenreUseCase {
+
+    @InjectMocks
+    private DefaultUpdateGenreUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @Mock
+    private GenreGateway genreGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateGenre_shouldReturnGenreId() {
+        // given
+        final var aGenre = Genre.newGenre("acao", true);
+
+        final var expectedId = aGenre.getId();
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var aCommand = UpdateGenreCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedIsActive,
+                expectedCategories
+        );
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+        when(genreGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
+
+        Mockito.verify(genreGateway, times(1)).findById(eq(expectedId));
+
+        Mockito.verify(genreGateway, times(1)).update(argThat(aUpdatedGenre ->
+                Objects.equals(expectedId, aUpdatedGenre.getId())
+                        && Objects.equals(expectedName, aUpdatedGenre.getName())
+                        && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
+                        && Objects.equals(expectedCategories, aUpdatedGenre.getCategories())
+                        && Objects.equals(aGenre.getCreatedAt(), aUpdatedGenre.getCreatedAt())
+                        && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
+                        && Objects.isNull(aUpdatedGenre.getDeletedAt())
+        ));
+    }
+}
