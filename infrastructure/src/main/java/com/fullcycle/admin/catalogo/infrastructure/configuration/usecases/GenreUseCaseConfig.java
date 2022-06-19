5c06ff02a0cc1f50234614a53a8ff970ffd946c8
gsuaki <gabriel.suaki@mercadolivre.com>
2022-06-19 19:28:39 -0300

    10.17-injetando-a-implementacao-do-gateway-nos-casos-de-uso

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/usecases/GenreUseCaseConfig.java
@@ -0,0 +1,58 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration.usecases;
+
+import com.fullcycle.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.create.DefaultCreateGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.delete.DefaultDeleteGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.delete.DeleteGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.get.DefaultGetGenreByIdUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.list.DefaultListGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.retrieve.list.ListGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.update.DefaultUpdateGenreUseCase;
+import com.fullcycle.admin.catalogo.application.genre.update.UpdateGenreUseCase;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class GenreUseCaseConfig {
+
+    private final CategoryGateway categoryGateway;
+    private final GenreGateway genreGateway;
+
+    public GenreUseCaseConfig(
+            final CategoryGateway categoryGateway,
+            final GenreGateway genreGateway
+    ) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+        this.genreGateway = Objects.requireNonNull(genreGateway);
+    }
+
+    @Bean
+    public CreateGenreUseCase createGenreUseCase() {
+        return new DefaultCreateGenreUseCase(categoryGateway, genreGateway);
+    }
+
+    @Bean
+    public DeleteGenreUseCase deleteGenreUseCase() {
+        return new DefaultDeleteGenreUseCase(genreGateway);
+    }
+
+    @Bean
+    public GetGenreByIdUseCase getGenreByIdUseCase() {
+        return new DefaultGetGenreByIdUseCase(genreGateway);
+    }
+
+    @Bean
+    public ListGenreUseCase listGenreUseCase() {
+        return new DefaultListGenreUseCase(genreGateway);
+    }
+
+    @Bean
+    public UpdateGenreUseCase updateGenreUseCase() {
+        return new DefaultUpdateGenreUseCase(categoryGateway, genreGateway);
+    }
+}
