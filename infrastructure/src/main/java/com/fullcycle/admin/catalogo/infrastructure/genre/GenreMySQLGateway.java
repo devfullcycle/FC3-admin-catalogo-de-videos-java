dba85e07a55abe5d041b272bea3b876d16ac6347
gsuaki <gabriel.suaki@mercadolivre.com>
2022-06-19 18:21:51 -0300

    10.08-implementando-o-metodo-create-do-gateway-de-genre

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
@@ -5,16 +5,25 @@ import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
 import com.fullcycle.admin.catalogo.domain.genre.GenreID;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
 
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
-    public Genre create(Genre aGenre) {
-        return null;
+    public Genre create(final Genre aGenre) {
+        return save(aGenre);
     }
 
     @Override
@@ -35,5 +44,10 @@ public class GenreMySQLGateway implements GenreGateway {
     @Override
     public Pagination<Genre> findAll(SearchQuery aQuery) {
         return null;
+    }
+
+    private Genre save(final Genre aGenre) {
+        return this.genreRepository.save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
     }
 }
