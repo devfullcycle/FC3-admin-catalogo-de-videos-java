c41fbb03a44c61f99a47d14e0925c70afe2b1fe4
gsuaki <gabriel.suaki@mercadolivre.com>
2022-06-27 13:53:33 -0300

    11.5-testes-integrados-do-caso-de-uso-de-delete-de-genero

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/genre/delete/DeleteGenreUseCaseIT.java
@@ -0,0 +1,55 @@
+package com.fullcycle.admin.catalogo.application.genre.delete;
+
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.genre.Genre;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import com.fullcycle.admin.catalogo.domain.genre.GenreID;
+import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class DeleteGenreUseCaseIT {
+
+    @Autowired
+    private DeleteGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldDeleteGenre() {
+        // given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = aGenre.getId();
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        Assertions.assertEquals(0, genreRepository.count());
+    }
+
+    @Test
+    public void givenAnInvalidGenreId_whenCallsDeleteGenre_shouldBeOk() {
+        // given
+        genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = GenreID.from("123");
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        Assertions.assertEquals(1, genreRepository.count());
+    }
+}
