0844de6d5c9bf8fefa8ed1fd57bd844b376277bf
gsuaki <gabriel.suaki@mercadolivre.com>
2022-07-03 15:20:05 -0300

    12.03-implementando-o-endpoint-de-criacao-de-genero

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/GenreController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/GenreController.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
+import com.fullcycle.admin.catalogo.application.genre.create.CreateGenreCommand;
+import com.fullcycle.admin.catalogo.application.genre.create.CreateGenreUseCase;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.infrastructure.api.GenreAPI;
 import com.fullcycle.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
@@ -9,12 +11,28 @@ import com.fullcycle.admin.catalogo.infrastructure.genre.models.UpdateGenreReque
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 public class GenreController implements GenreAPI {
 
+    private final CreateGenreUseCase createGenreUseCase;
+
+    public GenreController(final CreateGenreUseCase createGenreUseCase) {
+        this.createGenreUseCase = createGenreUseCase;
+    }
+
     @Override
     public ResponseEntity<?> create(final CreateGenreRequest input) {
-        return null;
+        final var aCommand = CreateGenreCommand.with(
+                input.name(),
+                input.isActive(),
+                input.categories()
+        );
+
+        final var output = this.createGenreUseCase.execute(aCommand);
+
+        return ResponseEntity.created(URI.create("/genres/" + output.id())).body(output);
     }
 
     @Override
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/models/CreateGenreRequest.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/models/CreateGenreRequest.java
@@ -2,6 +2,7 @@ package com.fullcycle.admin.catalogo.infrastructure.genre.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.List;
 
 public record CreateGenreRequest(
@@ -9,4 +10,12 @@ public record CreateGenreRequest(
         @JsonProperty("categories_id") List<String> categories,
         @JsonProperty("is_active") Boolean active
 ) {
+
+    public boolean isActive() {
+        return this.active != null ? this.active : true;
+    }
+
+    public List<String> categories() {
+        return this.categories != null ? this.categories : Collections.emptyList();
+    }
 }
