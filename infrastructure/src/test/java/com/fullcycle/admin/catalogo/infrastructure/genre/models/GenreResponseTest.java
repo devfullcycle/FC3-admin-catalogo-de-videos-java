e9825ddc53312e24b388a881ce66440325f21dca
gsuaki <gabriel.suaki@mercadolivre.com>
2022-07-03 16:12:32 -0300

    12.12-testes-de-serializacao-da-classe-genreresponse

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/genre/models/GenreResponseTest.java
@@ -0,0 +1,91 @@
+package com.fullcycle.admin.catalogo.infrastructure.genre.models;
+
+import com.fullcycle.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+import java.util.List;
+
+@JacksonTest
+public class GenreResponseTest {
+
+    @Autowired
+    private JacksonTester<GenreResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Ação";
+        final var expectedCategories = List.of("123");
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new GenreResponse(
+                expectedId,
+                expectedName,
+                expectedCategories,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedUpdatedAt,
+                expectedDeletedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.categories_id", expectedCategories)
+                .hasJsonPathValue("$.is_active", expectedIsActive)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt.toString())
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt.toString())
+                .hasJsonPathValue("$.updated_at", expectedUpdatedAt.toString());
+    }
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Ação";
+        final var expectedCategory = "456";
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var json = """
+                {
+                  "id": "%s",
+                  "name": "%s",
+                  "categories_id": ["%s"],
+                  "is_active": %s,
+                  "created_at": "%s",
+                  "deleted_at": "%s",
+                  "updated_at": "%s"
+                }    
+                """.formatted(
+                expectedId,
+                expectedName,
+                expectedCategory,
+                expectedIsActive,
+                expectedCreatedAt.toString(),
+                expectedDeletedAt.toString(),
+                expectedUpdatedAt.toString()
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("id", expectedId)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("categories", List.of(expectedCategory))
+                .hasFieldOrPropertyWithValue("active", expectedIsActive)
+                .hasFieldOrPropertyWithValue("createdAt", expectedCreatedAt)
+                .hasFieldOrPropertyWithValue("deletedAt", expectedDeletedAt)
+                .hasFieldOrPropertyWithValue("updatedAt", expectedUpdatedAt);
+    }
+}
