4f0799cc634c27d532f922e90117ac3ab6e31737
gsuaki <gabriel.suaki@mercadolivre.com>
2022-07-24 21:00:46 -0300

    14.8-criar-a-interface-do-gateway-de-castmember

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/castmember/CastMember.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/castmember/CastMember.java
@@ -36,6 +36,26 @@ public class CastMember extends AggregateRoot<CastMemberID> {
         return new CastMember(anId, aName, aType, now, now);
     }
 
+    public static CastMember with(
+            final CastMemberID anId,
+            final String aName,
+            final CastMemberType aType,
+            final Instant aCreationDate,
+            final Instant aUpdateDate
+    ) {
+        return new CastMember(anId, aName, aType, aCreationDate, aUpdateDate);
+    }
+
+    public static CastMember with(final CastMember aMember) {
+        return new CastMember(
+                aMember.id,
+                aMember.name,
+                aMember.type,
+                aMember.createdAt,
+                aMember.updatedAt
+        );
+    }
+
     public CastMember update(final String aName, final CastMemberType aType) {
         this.name = aName;
         this.type = aType;
