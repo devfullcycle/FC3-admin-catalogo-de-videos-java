359c3065779410489f43b2d73b807c64bfb6e878
gsuaki <gabriel.suaki@mercadolivre.com>
2022-07-31 20:08:24 -0300

    15.07-testes-unitarios-do-caso-de-uso-de-delete-de-castmember-por-id

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/castmember/delete/DeleteCastMemberUseCaseTest.java
@@ -0,0 +1,80 @@
+package com.fullcycle.admin.catalogo.application.castmember.delete;
+
+import com.fullcycle.admin.catalogo.application.Fixture;
+import com.fullcycle.admin.catalogo.application.UseCaseTest;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMember;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class DeleteCastMemberUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultDeleteCastMemberUseCase useCase;
+
+    @Mock
+    private CastMemberGateway castMemberGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(castMemberGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCastMember_shouldDeleteIt() {
+        // given
+        final var aMember = CastMember.newMember(Fixture.name(), Fixture.CastMember.type());
+
+        final var expectedId = aMember.getId();
+
+        doNothing()
+                .when(castMemberGateway).deleteById(any());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAnInvalidId_whenCallsDeleteCastMember_shouldBeOk() {
+        // given
+        final var expectedId = CastMemberID.from("123");
+
+        doNothing()
+                .when(castMemberGateway).deleteById(any());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCastMemberAndGatewayThrowsException_shouldReceiveException() {
+        // given
+        final var aMember = CastMember.newMember(Fixture.name(), Fixture.CastMember.type());
+
+        final var expectedId = aMember.getId();
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(castMemberGateway).deleteById(any());
+
+        // when
+        Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+    }
+}
