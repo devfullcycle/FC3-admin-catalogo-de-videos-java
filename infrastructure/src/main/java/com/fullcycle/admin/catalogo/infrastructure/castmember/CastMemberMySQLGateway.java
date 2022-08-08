acea2f466c226eb890e20036eadfb089e2ad95b5
gsuaki <gabriel.suaki@mercadolivre.com>
2022-08-08 08:05:42 -0300

    16.13-implementando-o-metodo-findbyid-do-gateway-de-castmember

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/castmember/CastMemberMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/castmember/CastMemberMySQLGateway.java
@@ -36,7 +36,8 @@ public class CastMemberMySQLGateway implements CastMemberGateway {
 
     @Override
     public Optional<CastMember> findById(final CastMemberID anId) {
-        return Optional.empty();
+        return this.castMemberRepository.findById(anId.getValue())
+                .map(CastMemberJpaEntity::toAggregate);
     }
 
     @Override
