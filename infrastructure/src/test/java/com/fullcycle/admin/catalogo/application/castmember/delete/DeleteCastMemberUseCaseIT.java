6930bc72cd59e1c779a85dc7e9ce30ad73b7e614
gsuaki <gabriel.suaki@mercadolivre.com>
2022-08-16 08:02:26 -0300

    17.3-testes-integrados-do-caso-de-uso-de-delete-de-castmember

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/castmember/delete/DeleteCastMemberUseCaseIT.java
@@ -0,0 +1,100 @@
+package com.fullcycle.admin.catalogo.application.castmember.delete;
+
+import com.fullcycle.admin.catalogo.Fixture;
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMember;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalogo.infrastructure.castmember.persistence.CastMemberJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+@IntegrationTest
+public class DeleteCastMemberUseCaseIT {
+
+    @Autowired
+    private DeleteCastMemberUseCase useCase;
+
+    @Autowired
+    private CastMemberRepository castMemberRepository;
+
+    @SpyBean
+    private CastMemberGateway castMemberGateway;
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCastMember_shouldDeleteIt() {
+        // given
+        final var aMember = CastMember.newMember(Fixture.name(), Fixture.CastMember.type());
+        final var aMemberTwo = CastMember.newMember(Fixture.name(), Fixture.CastMember.type());
+
+        final var expectedId = aMember.getId();
+
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember));
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMemberTwo));
+
+        Assertions.assertEquals(2, this.castMemberRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+        Assertions.assertFalse(this.castMemberRepository.existsById(expectedId.getValue()));
+        Assertions.assertTrue(this.castMemberRepository.existsById(aMemberTwo.getId().getValue()));
+    }
+
+    @Test
+    public void givenAnInvalidId_whenCallsDeleteCastMember_shouldBeOk() {
+        // given
+        this.castMemberRepository.saveAndFlush(
+                CastMemberJpaEntity.from(
+                        CastMember.newMember(Fixture.name(), Fixture.CastMember.type())
+                )
+        );
+
+        final var expectedId = CastMemberID.from("123");
+
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCastMemberAndGatewayThrowsException_shouldReceiveException() {
+        // given
+        final var aMember = CastMember.newMember(Fixture.name(), Fixture.CastMember.type());
+
+        this.castMemberRepository.saveAndFlush(CastMemberJpaEntity.from(aMember));
+
+        final var expectedId = aMember.getId();
+
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(castMemberGateway).deleteById(any());
+
+        // when
+        Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+
+        // then
+        verify(castMemberGateway).deleteById(eq(expectedId));
+
+        Assertions.assertEquals(1, this.castMemberRepository.count());
+    }
+}
