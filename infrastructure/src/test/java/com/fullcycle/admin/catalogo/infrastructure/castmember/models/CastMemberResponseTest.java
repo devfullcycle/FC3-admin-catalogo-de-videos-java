1e70bc759b33edf7f5f1ef78f3b170490628f281
gsuaki <gabriel.suaki@mercadolivre.com>
2022-08-19 08:19:30 -0300

    18.12-testes-de-serializacao-da-classe-castmemberresponse

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/castmember/models/CastMemberResponseTest.java
@@ -0,0 +1,43 @@
+package com.fullcycle.admin.catalogo.infrastructure.castmember.models;
+
+import com.fullcycle.admin.catalogo.Fixture;
+import com.fullcycle.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+class CastMemberResponseTest {
+
+    @Autowired
+    private JacksonTester<CastMemberResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type().name();
+        final var expectedCreatedAt = Instant.now().toString();
+        final var expectedUpdatedAt = Instant.now().toString();
+
+        final var response = new CastMemberResponse(
+                expectedId,
+                expectedName,
+                expectedType,
+                expectedCreatedAt,
+                expectedUpdatedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.type", expectedType)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt)
+                .hasJsonPathValue("$.updated_at", expectedUpdatedAt);
+    }
+}
