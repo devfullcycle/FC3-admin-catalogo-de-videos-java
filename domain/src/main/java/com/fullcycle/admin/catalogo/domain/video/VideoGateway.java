b4e92b96d53ef2a6e99f2dc5ac40e7266ffcee5e
gsuaki <gabriel.suaki@mercadolivre.com>
2022-09-09 02:20:20 -0300

    20.10-criar-a-interface-do-gateway-de-video

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/VideoGateway.java
@@ -0,0 +1,19 @@
+package com.fullcycle.admin.catalogo.domain.video;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface VideoGateway {
+
+    Video create(Video aVideo);
+
+    void deleteById(VideoID anId);
+
+    Optional<Video> findById(VideoID anId);
+
+    Video update(Video aVideo);
+
+    Pagination<Video> findAll(VideoSearchQuery aQuery);
+
+}
