f4749aae61f75c716a90f883fe2d9550ac1564ad
gsuaki <gabriel.suaki@mercadolivre.com>
2022-09-18 23:04:07 -0300

    21.13-implementando-o-caso-de-uso-de-listagem-de-video

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/retrieve/list/VideoListOutput.java
@@ -0,0 +1,24 @@
+package com.fullcycle.admin.catalogo.application.video.retrieve.list;
+
+import com.fullcycle.admin.catalogo.domain.video.Video;
+
+import java.time.Instant;
+
+public record VideoListOutput(
+        String id,
+        String title,
+        String description,
+        Instant createdAt,
+        Instant updatedAt
+) {
+
+    public static VideoListOutput from(final Video aVideo) {
+        return new VideoListOutput(
+                aVideo.getId().getValue(),
+                aVideo.getTitle(),
+                aVideo.getDescription(),
+                aVideo.getCreatedAt(),
+                aVideo.getUpdatedAt()
+        );
+    }
+}
