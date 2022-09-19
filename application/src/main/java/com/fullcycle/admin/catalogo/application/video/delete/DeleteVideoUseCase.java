524fcd8a863e2b7a948f2b635f1b5271fb5e2178
gsuaki <gabriel.suaki@mercadolivre.com>
2022-09-18 21:36:24 -0300

    21.06-implementando-o-caso-de-uso-de-delete-de-video-por-id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/delete/DeleteVideoUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalogo.application.video.delete;
+
+import com.fullcycle.admin.catalogo.application.UnitUseCase;
+
+public abstract class DeleteVideoUseCase extends UnitUseCase<String> {
+}
