af25e194cead4b729a985ced62fcb16cc33c0113
gsuaki <gabriel.suaki@mercadolivre.com>
2022-09-18 22:04:47 -0300

    21.08-implementando-o-caso-de-uso-de-get-de-video-por-id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/retrieve/get/GetVideoByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalogo.application.video.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetVideoByIdUseCase extends UseCase<String, VideoOutput> {
+}
