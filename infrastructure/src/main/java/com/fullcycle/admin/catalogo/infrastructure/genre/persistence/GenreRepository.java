444524b6b92cbdbcb30ee2c8a5447ce7904d1b2d
deploydesexta <gabriel.suaki@mercadolivre.com>
2023-01-10 21:45:20 -0300

    22.27-implementando-o-metodo-existsByIds-do-genregateway

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreRepository.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface GenreRepository extends JpaRepository<GenreJpaEntity, String> {
 
     Page<GenreJpaEntity> findAll(Specification<GenreJpaEntity> whereClause, Pageable page);
+
+    @Query(value = "select g.id from Genre g where g.id in :ids")
+    List<String> existsByIds(@Param("ids") List<String> ids);
 }
--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/genre/GenreMySQLGatewayTest.java
@@ -315,6 +315,26 @@ public class GenreMySQLGatewayTest {
     }
 
     @Test
+    public void givenTwoGenresAndOnePersisted_whenCallsExistsByIds_shouldReturnPersistedID() {
+        // given
+        final var aGenre = Genre.newGenre("Genre 1", true);
+
+        final var expectedItems = 1;
+        final var expectedId = aGenre.getId();
+
+        Assertions.assertEquals(0, genreRepository.count());
+
+        genreRepository.saveAndFlush(GenreJpaEntity.from(aGenre));
+
+        // when
+        final var actualGenre = genreGateway.existsByIds(List.of(GenreID.from("123"), expectedId));
+
+        // then
+        Assertions.assertEquals(expectedItems, actualGenre.size());
+        Assertions.assertEquals(expectedId.getValue(), actualGenre.get(0).getValue());
+    }
+
+    @Test
     public void givenAPrePersistedGenre_whenCallsDeleteById_shouldDeleteGenre() {
         // given
         final var aGenre = Genre.newGenre("Ação", true);
