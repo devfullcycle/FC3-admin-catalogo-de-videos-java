d74515b593659fd6e00f9f82c036b2f94763d1d1
deploydesexta <gabriel.suaki@mercadolivre.com>
2023-04-10 19:19:12 -0300

    32.07-visualizando-keyvalue-pairs-com-kv-filter-do-logstash

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,6 +1,8 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
@@ -8,8 +10,12 @@ import org.springframework.core.env.AbstractEnvironment;
 @SpringBootApplication
 public class Main {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOG.info("[step:to-be-init] [id:1] Inicializando o Spring");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+        LOG.info("[step:inittialized] [id:2] Spring inicializado..");
     }
 }
