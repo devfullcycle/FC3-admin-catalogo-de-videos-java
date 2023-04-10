4af772a84be5f36505af576e5ff2db41e1b91e8a
deploydesexta <gabriel.suaki@mercadolivre.com>
2023-04-10 19:01:43 -0300

    32.05-adicionando-o-filebeat

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -3,9 +3,11 @@ docker network create adm_videos_services
 docker network create elastic
 
 # Criar as pastas com permissões
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/es01
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/filebeat
 
 docker compose -f services/docker-compose.yml up -d
 docker compose -f elk/docker-compose.yml up -d
