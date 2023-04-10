2b2a0f402f4f531ada458c9d764111f574ae6ff2
deploydesexta <gabriel.suaki@mercadolivre.com>
2023-04-10 17:42:12 -0300

    31.04-criando-a-estrutura-de-sandbox-com-docker

---
+++ sandbox/run.sh
@@ -0,0 +1,9 @@
+
+# Criar as pastas com permissões
+mkdir -m 777 .docker
+mkdir -m 777 .docker/keycloak
+
+docker compose -f app/docker-compose.yml up -d
+
+echo "Inicializando os containers..."
+sleep 20
