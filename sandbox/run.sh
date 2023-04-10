7da9d0191c25741c2988fcff272168b2f7ad7eab
deploydesexta <gabriel.suaki@mercadolivre.com>
2023-04-10 18:37:50 -0300

    32.02-adicionando-o-elasticsearch-e-a-docker-network-elastic

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,11 +1,15 @@
 # Criar as docker networks
 docker network create adm_videos_services
+docker network create elastic
 
 # Criar as pastas com permissões
 mkdir -m 777 .docker
+mkdir -m 777 .docker/es01
 mkdir -m 777 .docker/keycloak
 
 docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+#docker compose -f app/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
